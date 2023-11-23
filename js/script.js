385a8e75fc599997676c77079a1f9e4156db25d1
Donato-Carola <don94@live.it>
2023-11-23 17:53:44 +0100

    seconda linea di pensiero

--- js/script.js
+++ js/script.js
@@ -14,9 +14,13 @@ Quanti millisecondi mi separano da domani alle 9:30?
 Esiste un oggetto JS in grado di gestire le date?
 Esistono dei metodi per trasformare una data in millisecondi?*/ 
 
-const Timer = document.getElementById(timerNewYear)
 
-const dataCapodanno = (new Date("January 01, 2024 00:00:00")).getTime();
+
+/*const dataCapodanno = (new Date("January 01, 2024 00:00:00")).getTime();
+const dataMillisecondi=new Date (dataCapodanno)
+//const dataa = dataMillisecondi.toLocaleTimeString()
+
+//console.log(`la data in millesecondi copiata in una variabile è ${dataMillisecondi}`)
 
 console.log(dataCapodanno);
 
@@ -26,7 +30,21 @@ console.log(dataNow);
 if(dataNow!==dataCapodanno){
    const risultato = (dataCapodanno-dataNow);
    console.log(`i millesecondi che ci separano dal capodanno sono ${risultato}`)
+   stampa.innerHTML=(`${risultato}`)
 }else{
     console.log(`buon anno`)
 
+}*/
+const stampa = document.querySelector('#spanStampa');
+const currentYear=new Date().getFullYear();
+const dataCapodanno=new Date(`January 1 ${currentYear+1} 00:00:00`);
+
+function aggiornamentoTime(){
+    const dataNow= new Date();
+    const risultato = dataCapodanno - dataNow;
+    console.log(risultato)
+    const ms =Math.floor(risultato%1000)
+    stampa.innerHTML=ms<10?"0"+ms:ms;
 }
+
+//setInterval(aggiornamentoTime,1000);
